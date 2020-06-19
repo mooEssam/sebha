441afd6dbcea66970cec085b0fbc114b66de9d39
mooEssam <anamemo945@gmail.com>
2020-06-19 23:57:14 +0200

    commit

--- app/src/main/java/com/example/seb7a/Masbaha.java
+++ app/src/main/java/com/example/seb7a/Masbaha.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -13,15 +15,19 @@ import android.widget.Toast;
 
 public class Masbaha extends AppCompatActivity {
     TextView sophan,hamd,akpar,result;
-    ImageButton plus,refin;
+    ImageButton plus,refin,history;
     EditText zekr;
     int number=0,memo=0;
+    int m;
+    SharedPreferences sharedPreferences;
 
     @SuppressLint("WrongConstant")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_masbaha);
+        sharedPreferences=getBaseContext().getSharedPreferences("memo",getBaseContext().MODE_PRIVATE);
+        history=findViewById(R.id.rusem);
         sophan=findViewById(R.id.sophan);
         hamd=findViewById(R.id.hamd);
         akpar=findViewById(R.id.akpar);
@@ -30,9 +36,10 @@ public class Masbaha extends AppCompatActivity {
         refin=findViewById(R.id.refine);
         zekr=findViewById(R.id.zekr);
         Intent i=getIntent();
-        int m=i.getIntExtra("i",0);
+         m=i.getIntExtra("i",0);
         if (m==1)
         {
+            history.setVisibility(View.INVISIBLE);
             zekr.setVisibility(View.INVISIBLE);
             plus.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -42,6 +49,25 @@ public class Masbaha extends AppCompatActivity {
                         number++;
                         memo++;
                         result.setText(number+"");
+                        if (memo<33){
+                            sophan.setVisibility(View.VISIBLE);
+                            hamd.setVisibility(View.INVISIBLE);
+                            akpar.setVisibility(View.INVISIBLE);
+                        }
+                        if (memo>33&&memo<=66)
+                        {
+                            sophan.setVisibility(View.VISIBLE);
+                            hamd.setVisibility(View.VISIBLE);
+                            akpar.setVisibility(View.INVISIBLE);
+
+                        }
+                        if (memo>66)
+                        {
+                            sophan.setVisibility(View.VISIBLE);
+                            hamd.setVisibility(View.VISIBLE);
+                            akpar.setVisibility(View.VISIBLE);
+
+                        }
                         if (memo==99){
                             Toast.makeText(getBaseContext(),"لقد اتممت تسبيح الصلاة",Toast.LENGTH_LONG).show();
                             startActivity(new Intent(Masbaha.this,MainActivity.class));
@@ -56,6 +82,7 @@ public class Masbaha extends AppCompatActivity {
 
                 }
             });
+
 
         }
 
@@ -81,7 +108,33 @@ public class Masbaha extends AppCompatActivity {
             }
         });
 
+        history.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                number=sharedPreferences.getInt("no",0);
+              result.setText(number+"");
+
+
+            }
+        });
+
 
     }
 
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (m==2)
+        {
+
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            int nom= Integer.parseInt(result.getText().toString());
+            editor.putInt("no", nom);
+            editor.apply();
+        }
+    }
 }
